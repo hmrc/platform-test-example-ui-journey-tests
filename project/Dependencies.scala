cae723e479acb32da17e08d3484abd473a2a5a64
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2023-11-22 16:10:37 +0000

    PLATUI - bump library version to test in labs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"   % "0.62.2" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.17" % Test,
     "org.slf4j"            % "slf4j-simple"   % "1.7.36" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.+"    % Test // Do NOT use .+ notation in test repositories
+    "uk.gov.hmrc"         %% "ui-test-runner" % "0.12.0" % Test // Do NOT use .+ notation in test repositories
   )
 
 }
