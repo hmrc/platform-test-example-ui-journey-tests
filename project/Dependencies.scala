f722c0b162e3e413e65a019cdb62ded46d2d16d1
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-02-23 11:46:42 +0000

    PLATUI-BAU: Update scalatest version to 3.2.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"   % "0.62.2" % Test,
-    "org.scalatest"       %% "scalatest"      % "3.2.17" % Test,
+    "org.scalatest"       %% "scalatest"      % "3.2.18" % Test,
     "org.slf4j"            % "slf4j-simple"   % "1.7.36" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner" % "0.+"    % Test // Do NOT use .+ notation in test repositories
   )
