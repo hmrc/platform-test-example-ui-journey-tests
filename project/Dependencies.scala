35e5b796bbc6960b6c3b9d7a3c5d44452ea2c7e7
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-03-01 15:31:07 +0000

    PLATUI-2886: Replace slf4j-simple with logback-classic version 1.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
-    "org.scalatest"       %% "scalatest"      % "3.2.18" % Test,
-    "org.slf4j"            % "slf4j-simple"   % "2.0.9"  % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.+"    % Test // Do NOT use .+ notation in test repositories
+    "ch.qos.logback"       % "logback-classic" % "1.5.1"  % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.18" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.+"    % Test // Do NOT use .+ notation in test repositories
   )
 
 }
