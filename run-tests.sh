18b2113e22db53b9681b9c0e29d2ca8726f0881b
Marti Floyd <5116631+martibutler@users.noreply.github.com>
2025-03-03 15:28:23 +0000

    PBD-4336 Test with browser mirror url

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,6 @@
 
 BROWSER=$1
 ENVIRONMENT=$2
+SE_BROWSER_MIRROR_URL=https://lab03.artefacts.tax.service.gov.uk/artifactory/storage-googleapis-com
 
 sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Dsecurity.assessment=false test testReport
